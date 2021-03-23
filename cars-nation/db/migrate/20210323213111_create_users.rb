ba61843efbb5fbdc02e196a9c718e3f165cacc5b
Ethan <ethanchristianwayne@gmail.com>
2021-03-23 17:43:27 -0400

    message

---
+++ cars-nation/db/migrate/20210323213111_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :image_url
+      t.string :password_digest
+      #signifies that we are not storing plain text passwords in the database
+      
+      t.timestamps null: false
+    end
+  end
+end
