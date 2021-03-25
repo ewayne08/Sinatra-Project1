5912472e726b8a0682546b657b0cdc089feef2b4
Ethan <ethanchristianwayne@gmail.com>
2021-03-25 17:18:52 -0400

    message

--- cars-nation/app/controllers/posts_controller.rb
+++ cars-nation/app/controllers/posts_controller.rb
@@ -16,4 +16,47 @@ class PostsController < ApplicationController
         erb :'posts/index'
     end
 
+    #Create- render a form for user to create a new post
+    get '/posts/new' do
+        redirect_if_not_logged_in
+        erb :'posts/new'
+    end
+
+    post '/posts' do
+        #recieves params user inputs to create new post
+        post = Post.new(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
+        if post.save
+        #^ .save triggers active record validations
+        #success message
+        flash[:message] = "Post created successfully!"
+         # after creating post, redirect to post show page
+         redirect "/posts/#{post.id}"
+        else
+            #error message using active record supplied messaging
+            flash[:error] = "Unable to create post: #{post.errors.full_messages.to_sentence}"
+            redirect "posts/new"
+           end
+       end
+
+
+       #To display a single post
+    get '/posts/:id' do
+        find_post
+        erb :'/posts/show'
+    end
+
+    #Update
+    #Edit button that takes us to form
+    #Render form to edit post
+    get '/posts/:id/edit' do
+        redirect_if_not_logged_in
+        find_post
+        if authorized_to_edit?(@post)
+        erb :'/posts/edit'
+        else
+        flash[:error] = "You are not authorized to edit this post!"
+        redirect "/posts/#{@post.id}"
+        end
+    end
+
 
