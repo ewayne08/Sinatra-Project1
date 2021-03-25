fba5cd3a423d68aec02c4a07daa33e4cf63ee03c
Ethan <ethanchristianwayne@gmail.com>
2021-03-25 16:45:43 -0400

    message

--- cars-nation/app/controllers/posts_controller.rb
+++ cars-nation/app/controllers/posts_controller.rb
@@ -4,13 +4,16 @@ class PostsController < ApplicationController
 
     #Read - showing all posts
     get '/posts' do
-
+         #attach the posts model so we can view all from double
     redirect_if_not_logged_in
     if params[:query]
         @posts = Post.search(params[:query.].capitalize)
     else
         @posts = Post.all.reverse_posts.
+        #render all the posts
     end
-    @post = Post.find_by_id(session[:id])
-    erb :'posts/index'
-end
+        @post  = Post.find_by_id(session[:id])
+        erb :'posts/index'
+    end
+
+
