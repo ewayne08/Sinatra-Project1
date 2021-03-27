efebc6b5f125c73942143e0bd6530ca1d18b5eaf
Ethan <ethanchristianwayne@gmail.com>
2021-03-27 09:56:16 -0400

    message

---
+++ cars-nation/db/seeds.rb
@@ -0,0 +1,8 @@
+#Where I will write my seeds/data
+
+adam_lz = User.create(username: "adamlzsupra", image_url: "https://img.youtube.com/vi/gZebnc1KnpE/0.jpg", password: "pw" )
+
+stradman = User.create(username: "stradman2021", image_url: "https://i.ytimg.com/vi/4n7s0PNXYas/maxresdefault.jpg", password: "pw" )
+
+jayleno = User.create(username: "jleno", image_url: "https://www.shearcomfort.com/blog/wp-content/uploads/Jay-Leno-Grage.jpg", password: "pw" )
+
