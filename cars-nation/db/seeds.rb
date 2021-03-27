2a1f59adea51b9205b93f2b1af3acd9bf20346d8
Ethan <ethanchristianwayne@gmail.com>
2021-03-27 10:26:17 -0400

    message

--- cars-nation/db/seeds.rb
+++ cars-nation/db/seeds.rb
@@ -7,4 +7,7 @@ the_stradman = User.create(username: "stradman2021", image_url: "https://i.ytimg
 jay_leno = User.create(username: "jleno", image_url: "https://www.shearcomfort.com/blog/wp-content/uploads/Jay-Leno-Grage.jpg", password: "pw" )
 
 Post.create(title: "Adam Lz Crazy Car Collection", image_url: "https://img.youtube.com/vi/gZebnc1KnpE/0.jpg", description: "Many people watch and look up to Adam Lz on youtube, with a car collection worth almost 3.5 million dollars. Adam Lz has all sorts of cars, drift cars, race cars, and all the goods", user_id: adam_lz.id)
+
 Post.create(title: "The Stradman Insane Car Collection", image_url: "https://i.ytimg.com/vi/4n7s0PNXYas/maxresdefault.jpg", description: "The Stradman one of the top youtubers with one of the nicest car collections. His car collection features some cars like a custom bugatti, and a supra", user_id: the_stradman.id)
+
+Post.create(title: "Jay Lenos 100 Million Dollar Car Collection", image_url: "https://www.shearcomfort.com/blog/wp-content/uploads/Jay-Leno-Grage.jpg", description: "Jay Leno one of the most famous people in the world also has one of the most insane car collections in the world, this boss garage has a $12 milliom dollar McLaren F1", user_id: jay_leno.id)
